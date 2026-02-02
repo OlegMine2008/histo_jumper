29e539bed7a7c9b5385221300ce9ccdaf2b410d7
OlegMine2008 <olegkosh08@gmail.com>
2026-02-02 07:37:13 +0300

    фпфшо

--- code/main.py
+++ code/main.py
@@ -1,9 +1,12 @@
-import platformer as g
+import arcade
 import menu as me
 
 
 def main():
-    pass
+    window = arcade.Window(1200, 600, "Just A Jumper")
+    menu_view = me.GameMenu()
+    window.show_view(menu_view)
+    arcade.run()
 
 if __name__ == '__main__':
     main()
